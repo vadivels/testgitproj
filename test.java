c23b7110fcf001dcfe2254b61caa4c9791b54967
vadivels <vvadivelin@gmail.com>
2020-07-21 14:01:49 -0400

    al

--- test.java
+++
@@ -1,7 +0,0 @@
- class test {
-
-    public static void main(String[] args) {
-        /* Enter your code here. Print output to STDOUT. Your class should be named Solution. */
-	System.out.println("This is test welcome page");
-    }
-}
---
+++ test1.java
@@ -0,0 +1,7 @@
+class test1 {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Print output to STDOUT. Your class should be named Solution. */
+	System.out.println("This is test welcome page");
+    }
+}
